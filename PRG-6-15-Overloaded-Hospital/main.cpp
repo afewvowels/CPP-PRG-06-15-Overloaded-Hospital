951bf03f329522e1762b00df57d814f619bc0a69
Keith Smith <aeiouandsometimesy@me.com>
2017-10-08 09:01:51 -0400

    Initial Commit

---
+++ PRG-6-15-Overloaded-Hospital/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-6-15-Overloaded-Hospital
+//
+//  Created by Keith Smith on 10/8/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
